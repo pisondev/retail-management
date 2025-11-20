497c57687cf63eda7c860f8fbcedb5ad3d4f47ae
Pison Golda <pison.gm.dev@gmail.com>
2025-11-20 08:51:52 +0700

    feat: setup dependency injection & route config for auth and user management

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"os"
+	"retail-management/app"
+	"retail-management/controller"
 	"retail-management/exception"
+	"retail-management/repository"
+	"retail-management/service"
 
 	_ "github.com/go-sql-driver/mysql"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
@@ -20,9 +25,22 @@ func main() {
 
 	serverPort := os.Getenv("SERVER_PORT")
 
+	db := app.NewDB()
+	validate := validator.New()
+
+	userRepository := repository.NewUserRepository(logger)
+	userService := service.NewUserService(userRepository, db, validate, logger)
+	userController := controller.NewUserController(userService, logger)
+
 	server := fiber.New(fiber.Config{
 		ErrorHandler: exception.ErrorHandler,
 	})
+
+	routeConfig := app.RouteConfig{
+		App:            server,
+		UserController: userController,
+	}
+	routeConfig.Setup()
 
 	err = server.Listen(serverPort)
 	if err != nil {
